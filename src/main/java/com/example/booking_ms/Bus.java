7c3392d01b202998e915522f44a4c903779ece0c
arun.rade <arun.rade@gmail.com>
2025-01-08 20:42:50 +0530

    Bus java class

--- src/main/java/com/example/booking_ms/Bus.java
+++ src/main/java/com/example/booking_ms/Bus.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class hotel {
-    @GetMapping("/hotel")
-    public String getData() {return  "Please book your hotel ticket in New Delhi 35% discount" ; }
+public class Bus {
+    @GetMapping("/Bus")
+    public String getData() {return  "Please book your Bus ticket in New Delhi 20% discount" ; }
 }
