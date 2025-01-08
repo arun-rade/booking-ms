c9e23905dc9c4c08331e5df51fb3e55c3453a3d5
arun-rade <arun.rade2110@gmail.com>
2025-01-08 20:41:51 +0530

    Update hotel.java

--- src/main/java/com/example/booking_ms/hotel.java
+++ src/main/java/com/example/booking_ms/hotel.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class hotel {
     @GetMapping("/hotel")
-    public String getData() {return  "Please book your hotel ticket in New Delhi 20% discount" ; }
+    public String getData() {return  "Please book your hotel ticket in New Delhi 50% discount" ; }
 }
