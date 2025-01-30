318405bf66f77d763141dd03a4c1d9058f530440
arun.rade <arun.rade@gmail.com>
2025-01-30 09:28:57 +0530

    discount of hotel and train chnages plus jar to war conversion

--- src/main/java/com/example/booking_ms/train.java
+++ src/main/java/com/example/booking_ms/train.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class train {
     @GetMapping("/train")
-    public String getData() {return  "Please book your Train ticket for New Delhi t 35% discount" ; }
+    public String getData() {return  "Please book your Train ticket for New Delhi t 45% discount" ; }
 }
