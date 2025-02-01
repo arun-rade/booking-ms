3c3b0fe10c5f37bd52b34e1f4d9ee5f199630935
arun.rade <arun.rade@gmail.com>
2025-02-01 20:29:57 +0530

    update bus java file as discout

--- src/main/java/com/example/booking_ms/Bus.java
+++ src/main/java/com/example/booking_ms/Bus.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Bus {
     @GetMapping("/Bus")
-    public String getData() {return  "Please book your Bus ticket in New Delhi 20% discount" ; }
+    public String getData() {return  "Please book your Bus ticket in New Delhi 30% discount" ; }
 }
