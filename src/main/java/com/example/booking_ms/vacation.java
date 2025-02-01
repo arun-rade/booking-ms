54998677a405e8ba7e675a69b291413f36c44704
arun.rade <arun.rade@gmail.com>
2025-02-01 19:26:06 +0530

    added new vacation java code

---
+++ src/main/java/com/example/booking_ms/vacation.java
@@ -0,0 +1,10 @@
+package com.example.booking_ms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class vacation {
+    @GetMapping("/vacation")
+    public String getData() {return  "Please book your vacation in New Delhi 20% discount" ; }
+}
